a133c79ec650d6c73e6a433532fc4e156e3b8cc5
Andreas Bertsatos <pr0m1th3as@users.noreply.github.com>
2018-08-04 14:24:18 +0300

    Add files via upload

---
+++ ABH077-Humerus_L.pp
@@ -0,0 +1,13 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="14:20:20" date="2018-08-04"/>
+  <User name="promitheas"/>
+  <DataFileName name="ABH077-Humerus_L.obj"/>
+  <templateName name=""/>
+ </DocumentData>
+ <point active="1" name="1" x="135.5340" y="3.8128" z="-46.5575"/>
+ <point active="1" name="2" x="147.7180" y="15.1417" z="-71.4764"/>
+ <point active="1" name="3" x="144.7990" y="-2.8793" z="-47.0134"/>
+ <point active="1" name="4" x="138.1860" y="10.5527" z="-73.6113"/>
+</PickedPoints>
