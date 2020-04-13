cc41eaee997b702f5d391432ef5969692f63af4f
Andreas Bertsatos <abertsatos@biol.uoa.gr>
2020-04-13 22:52:25 +0300

    changed copyright date

--- readObj.cc
+++ readObj.cc
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2018-2020 Andreas Bertsatos <andreas.bertsatos@gmail.com>
+Copyright (C) 2018-2020 Andreas Bertsatos <abertsatos@biol.uoa.gr>
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
