eb5d32aea9dadf665c35235b3eeb97c139f6d651
Andreas Bertsatos <abertsatos@biol.uoa.gr>
2022-06-05 08:26:05 +0300

    a script for compiling oct files

---
+++ compile.m
@@ -0,0 +1,7 @@
+mkoctfile longbone_maxDistance.cc
+mkoctfile longbone_Registration.cc
+mkoctfile meshArea.cc
+mkoctfile meshBarycenter.cc
+mkoctfile meshSection.cc
+mkoctfile readObj.cc
+mkoctfile writeObj.cc
